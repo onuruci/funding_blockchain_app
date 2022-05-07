48b120f165636dc75f283d00d9296cf88c0fbb93
onuruci <onur.yuce@metu.edu.tr>
2022-05-07 15:27:04 +0300

    url bug fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import react from "react";
 import Home from "./Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+require('dotenv').config();
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path={process.env.PUBLIC_URL +'/'} element={<Home/>}/>
           <Route path="/about" element={<div>Hiiiiiiiiiiiiiiiiii</div>} />
         </Routes>
       </BrowserRouter>
